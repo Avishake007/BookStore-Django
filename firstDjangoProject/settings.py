240481fc59b30e092882919fd5c29d581d317aa6
Avishake Maji <56298861+Avishake007@users.noreply.github.com>
2022-08-08 19:34:30 +0530

    Update settings.py

--- firstDjangoProject/settings.py
+++ firstDjangoProject/settings.py
@@ -127,13 +127,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #Added manually
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    
-]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 MEDIA_URL = '/bookDjango/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'bookDjango')
 CLOUDINARY_STORAGE = { 
