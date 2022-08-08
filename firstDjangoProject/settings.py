c3f1a5807727ac58db2f7a379ed880dbfc35e202
Avishake Maji <56298861+Avishake007@users.noreply.github.com>
2022-08-08 19:53:07 +0530

    Update settings.py

--- firstDjangoProject/settings.py
+++ firstDjangoProject/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['avi-book-store.herokuapp.com','127.0.0.1']
 
